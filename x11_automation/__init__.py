7edeb09484e669d67825ae4729a2f58cf13b8b94
jok4r <opensuse12@gmail.com>
2023-06-27 04:09:20 +0300

    Removed empty file x11_automation
    Edited __init__

--- x11_automation/__init__.py
+++ x11_automation/__init__.py
@@ -1,5 +1,5 @@
-from win_exists import win_exists, get_win_by_name
-from win_activate import win_activate
-from win_wait_active import win_wait_active
-from send import send
-from win_get_process import win_get_process
+from .win_exists import win_exists, get_win_by_name
+from .win_activate import win_activate
+from .win_wait_active import win_wait_active
+from .send import send
+from .win_get_process import win_get_process
