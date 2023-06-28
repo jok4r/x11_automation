2a14eb03efccad9110b564e3bbfddbf56c24fd53
jok4r <opensuse12@gmail.com>
2023-06-29 00:46:48 +0300

    Fixed send,py

--- x11_automation/send.py
+++ x11_automation/send.py
@@ -2,13 +2,15 @@ from pyKey import pressKey, releaseKey, press, sendSequence, showKeys
 import re
 
 
-def send(keys_str: str):
+def send(keys_str: str, sec_per_key=0.05):
     keys = []
     if matches := re.findall(r'{(\w+)}', keys_str):
         for match in matches:
-            keys.append(match[0].upper())
+            if match == 'BACKSPACE':
+                match = 'BKSP'
+            keys.append(match.upper())
     else:
         keys = [x for x in keys_str]
 
     for key in keys:
-        press(key)
+        press(key, sec=sec_per_key)
