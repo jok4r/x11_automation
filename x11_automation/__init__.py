2e84c644e86707035ba819c93e85f0ba8fdca5e3
jok4r <opensuse12@gmail.com>
2023-07-01 05:22:08 +0300

    Added function get_win_geometry

--- x11_automation/__init__.py
+++ x11_automation/__init__.py
@@ -4,4 +4,5 @@ from .win_wait_active import win_wait_active
 from .send import send
 from .win_get_process import win_get_process
 from .move_mouse_to_new_window import move_mouse_to_new_window
+from .get_win_geometry import get_win_geometry
 from .x11_automation import X11AutomationError
